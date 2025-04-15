a016d52cd5340d09e51d1c35fb812bd8f5888e04
Jakob Malmgren <Jakob.Malmgren1987@gmail.com>
2025-04-15 15:16:55 +0200

    crud

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,9 +1,23 @@
 import express from "express";
-import { createOrder } from "../controllers/orderController.js";
+import {
+  createOrder,
+  deleteOrder,
+  getMyOrder,
+  updateorder,
+} from "../controllers/orderController.js";
 
 const router = express.Router();
 
 //skapaOrder router
 router.post("/", createOrder);
+
+//deleteOrder router
+router.delete("/", deleteOrder);
+
+//getMyOrder router
+router.get("/", getMyOrder);
+
+//changeOrder router
+router.put("/", updateorder);
 
 export default router;
