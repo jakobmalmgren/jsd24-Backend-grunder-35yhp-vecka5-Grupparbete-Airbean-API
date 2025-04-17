6ef9e5f6c9cbe69276812dd0f6b7464264831e4d
robjor88 <robjor88@gmail.com>
2025-04-17 14:59:16 +0200

    added user validator

--- validators/userValidator.js
+++ validators/userValidator.js
@@ -6,7 +6,7 @@ import Joi from "joi";
 
 
 const createUserSchema = Joi.object({
-    username: Joi.string().min(1).required(),
+    username: Joi.alternatives().try(Joi.string(), Joi.number()).required(),
     password: Joi.alternatives().try(Joi.string(), Joi.number()).required(),
 }).options({ stripUnknown: true}) // matar man in nått annat än username / password filtreras det bort.
 
@@ -14,7 +14,7 @@ const createUserSchema = Joi.object({
 // username: kollar att username är en sträng , måste vara med
 //password: godkänner både en sträng & eller ett number.
 const loginUserSchema = Joi.object({
-    username: Joi.string().required(),
+    username: Joi.alternatives().try(Joi.string(), Joi.number()).required(),
     password: Joi.alternatives().try(Joi.string(), Joi.number()).required(),
 })
 
