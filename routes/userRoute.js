7c3bbefd8df8e7b18f3b32943b2aafc13ce58dee
Jakob Malmgren <Jakob.Malmgren1987@gmail.com>
2025-04-21 10:27:31 +0200

    kommentera ut i userroute

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -9,16 +9,13 @@ import { validateBody } from "../middlewares/validateBody.js";
 
 const router = express.Router();
 
-<<<<<<< HEAD
 //generell beskrivning av filen:
 //validerar om inkommande req.body har rätt format
 // samt skapar och loggar in genom usercontroller
 
 router.post("/create", validateBody(createUserSchema), createUser);
 router.post("/login", validateBody(loginUserSchema), loginUser);
-=======
-router.post("/create",validateBody(createUserSchema) , createUser);
-router.post("/login",validateBody(loginUserSchema), loginUser);
->>>>>>> 9b1d523efd05c3fa4a2d986b76209804813b313f
+// router.post("/create",validateBody(createUserSchema) , createUser);
+// router.post("/login",validateBody(loginUserSchema), loginUser);
 
 export default router;
