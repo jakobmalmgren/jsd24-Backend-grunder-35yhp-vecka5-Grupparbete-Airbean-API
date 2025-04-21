582c72d106c2b75e0a242fcc5561f2c9de8442ee
robjor88 <robjor88@gmail.com>
2025-04-21 11:57:17 +0200

    added route error handler & global error

--- server.js
+++ server.js
@@ -29,6 +29,20 @@ app.use("/api/history", historyRoute);
 //lägga beställning(köper, bekräfta beställning) (POST)
 app.use("/api/status", cartStatusRoute);
 
+
+// fångar upp om ingen route hittas
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Routen hittades inte" });
+});
+
+// Global error handler
+//fångar upp övriga fel
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Serverfel" });
+});
+
+
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
